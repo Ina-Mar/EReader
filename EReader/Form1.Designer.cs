80edc9be7a1149718835f2ec29ecd2a4bd2582bb
Ina-Mar <ina.martinkute@vvk.lt>
2022-12-04 18:37:32 +0200

    Add project files.

---
+++ EReader/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace EReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.webView22 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rasti knygą";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(270, 18);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(813, 547);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // webView22
+            // 
+            this.webView22.AllowExternalDrop = true;
+            this.webView22.CreationProperties = null;
+            this.webView22.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView22.Location = new System.Drawing.Point(630, 95);
+            this.webView22.Name = "webView22";
+            this.webView22.Size = new System.Drawing.Size(75, 23);
+            this.webView22.TabIndex = 2;
+            this.webView22.ZoomFactor = 1D;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(270, 573);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<< Atgal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Location = new System.Drawing.Point(913, 573);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Pirmyn >>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.Control;
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(12, 134);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(252, 431);
+            this.treeView1.TabIndex = 6;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 619);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webView22);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView22;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EReader/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VersOne.Epub;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System.IO;
+using System.IO.Compression;
+using VersOne.Epub.Environment;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace EReader
+{
+    public partial class Form1 : Form
+    {
+        EpubBook book;
+        int currentNavigationPage = 0;
+        private string GetFile()
+        {
+            string filePath = "";
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                InitialDirectory = @"G:\\",
+                Title = "Ieškoti EPUB",
+                DefaultExt = "epub",
+                CheckFileExists = true,
+                CheckPathExists = true, 
+                Filter = "epub files (*.epub)|*.epub",
+                FilterIndex = 0,
+
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+            }
+            return filePath;
+        }
+
+        private EpubBook OpenBook()
+        { 
+            string filePath = GetFile();
+            try
+            {
+                book = EpubReader.ReadBook(filePath);
+               
+            }
+            catch(AggregateException) 
+            {
+                MessageBox.Show("Negalima atverti knygos. Netinkama Epub versija");
+         
+            }
+            return book;
+        }
+
+        private string ExtractionPath(EpubBook book)
+        {
+            string filePath = book.FilePath;
+            string desPath = null;
+            if (filePath != null)
+            {
+                int index = filePath.LastIndexOf("\\");
+                int nameLength = filePath.Length - index - 5;
+                string tempDirectory = filePath.Substring(index + 1, nameLength);
+                string workDir = Directory.GetCurrentDirectory();
+                desPath = @workDir + "\\temp\\" + tempDirectory;
+            }
+            return desPath;
+        }
+        private void ExtractBook(EpubBook book, string desPath)
+        {
+            string filePath = book.FilePath;
+           
+            if (!Directory.Exists(desPath))
+            {
+                Directory.CreateDirectory(desPath);
+                ZipFile.ExtractToDirectory(filePath, desPath);
+                    
+            }
+          
+        }
+        private List<string> GetNavigationItemPath(List<EpubNavigationItem> navigationItems, int numInItems, int numInNestedItems)
+        {
+            string docPath;
+            List<string> navPath = new List<string>();
+            EpubTextContentFile contentFile;
+            if (numInNestedItems == -1)
+            {
+                contentFile = navigationItems[numInItems].HtmlContentFile;
+            }
+            else
+            {
+                contentFile = navigationItems[numInItems].NestedItems[numInNestedItems].HtmlContentFile;
+            }
+            EpubNavigationItemLink link = navigationItems[numInItems].Link;
+            string idPath = link.Anchor;
+            docPath = ExtractionPath(book) + "\\" + contentFile.FilePathInEpubArchive;
+            navPath.Add(docPath);
+            navPath.Add(contentFile.FileName);
+            navPath.Add(idPath);
+            return navPath;
+        }
+
+        private string GetPagePath(int numInList)
+        {
+            EpubTextContentFile contentFile = book.ReadingOrder[numInList];
+            string docPath = ExtractionPath(book) + "\\" + contentFile.FilePathInEpubArchive;
+            return docPath;
+        }
+
+        private List<EpubNavigationItem> GetPlainNavigation(List<EpubNavigationItem> bookItems)
+        {
+            List<EpubNavigationItem> navigationItems = new List<EpubNavigationItem>();
+            foreach (EpubNavigationItem item in bookItems)
+            {
+                if (item.NestedItems == null)
+                {
+                    navigationItems.Add(item);
+                }
+                else
+                {
+                    foreach (EpubNavigationItem nestedItem in item.NestedItems)
+                    {
+                        navigationItems.Add(nestedItem);
+                    }
+                }
+                
+
+            }
+            return navigationItems;
+        }
+
+        private void NavigationTree(List<EpubNavigationItem> navigationItems)
+        {
+            int numInList = 0;
+            foreach (EpubNavigationItem item in navigationItems)
+            {
+                treeView1.Nodes.Add(item.Title);
+                if (item.NestedItems != null)
+                {
+                    foreach (EpubNavigationItem nestedItem in item.NestedItems)
+                    {
+                        treeView1.Nodes[numInList].Nodes.Add(nestedItem.Title);
+                    }
+                }
+                numInList++;
+            }
+        }
+
+
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView21.Size = this.ClientSize - new Size(webView21.Location);
+            webView21.Height -= 40;
+            button2.Top = this.ClientSize.Height - button2.Height - 10;
+            button3.Top = this.ClientSize.Height - button2.Height - 10;
+            button3.Left = this.ClientSize.Width - button3.Width - 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string workDir = Directory.GetCurrentDirectory();
+            string des_path = @workDir + "\\temp";
+            if (!Directory.Exists(des_path))
+            {
+                Directory.CreateDirectory(des_path);
+            }
+            InitBrowser();
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+
+        private async Task Initizated()
+        {
+            await webView21.EnsureCoreWebView2Async(null);        
+        }
+
+        public async void InitBrowser()
+        {
+            await Initizated();
+            
+            
+
+        }
+
+        public async Task AddScript(string idRef)
+        {
+            await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("document.getElementById("+ idRef + ").scrollIntoView({behavior: 'smooth'});");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EpubBook book = OpenBook();
+            if (book != null)
+            {
+                string extPath = ExtractionPath(book);
+                ExtractBook(book, extPath);
+                EpubTextContentFile contentFile = book.ReadingOrder[currentNavigationPage];
+                string coverPage = GetPagePath(0);
+                webView21.CoreWebView2.Navigate(coverPage);
+                button2.Visible = true;
+                button3.Visible = true;
+                button2.Enabled = false;
+                NavigationTree(book.Navigation);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string page = "";
+            currentNavigationPage++;
+            if (currentNavigationPage <= book.ReadingOrder.Count-1)
+            {
+                 page = GetPagePath(currentNavigationPage);
+
+                webView21.CoreWebView2.Navigate(page);
+                //currentNavigationPage++;
+            }
+            if (currentNavigationPage == book.ReadingOrder.Count-1)
+            {
+                button3.Enabled = false;
+            }
+            button2.Enabled=true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentNavigationPage--;
+            string page = "";
+            if (currentNavigationPage >= 0)
+            {
+                page = GetPagePath(currentNavigationPage);
+
+                webView21.CoreWebView2.Navigate(page);
+                
+            }
+            if (currentNavigationPage == 0)
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            
+        }
+
+        private async void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            int parentSelection, childSelection;
+            childSelection = -1;
+            if (e.Node.Parent == null)
+            {
+                parentSelection = e.Node.Index;
+            }
+            else
+            {
+                parentSelection = e.Node.Parent.Index;
+                childSelection = e.Node.Index;
+            }
+            List<String> pageInfo = GetNavigationItemPath(book.Navigation, parentSelection, childSelection);
+            webView21.CoreWebView2.Navigate(pageInfo[0]);
+            label1.Text = pageInfo[2];
+           
+            for (int i = 0; i < book.ReadingOrder.Count(); i++)
+            {
+                if (book.ReadingOrder[i].FileName == pageInfo[1])
+                {
+                    await AddScript(pageInfo[2]);
+                    currentNavigationPage = i;
+                    break;
+                }
+            }
+        }
+    }
+}
